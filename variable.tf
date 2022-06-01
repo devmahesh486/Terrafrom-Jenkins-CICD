33c482f64904de2da38c556adffaca912f5be3b7
devmahesh486 <97374063+devmahesh486@users.noreply.github.com>
2022-06-01 21:35:05 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "access_key" {
-  default = "KIA6HCXMEU4GH4FZN4U"
+  default = "KIA"
   }
 variable "secret_key" {
-  default = "iyhSgNJxEuCGa847+qbJ9z3pE8KD+SvHjRijI42U"
+  default = "iyhSgNJxE"
   }
 variable "region" {
   default = "ap-south-1"
